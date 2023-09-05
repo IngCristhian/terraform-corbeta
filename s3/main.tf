e35bed42c8c84ef2eb0d401490336571c592e12c
Alvis <cristian.david.alvis@accenture.com>
2023-09-05 12:35:45 -0500

    update file

--- s3/main.tf
+++ s3/main.tf
@@ -6,5 +6,4 @@ variable "bucket_names" {
 resource "aws_s3_bucket" "buckets" {
     count         = length(var.bucket_names)
     bucket        = var.bucket_names[count.index]
-    acl           = "private"  
 }
